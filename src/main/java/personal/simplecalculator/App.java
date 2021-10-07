c6334f386b3d46943099da4a1762bbf43c1de4f9
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-07 17:05:24 +0200

    Added graphics class and simple fx

--- src/main/java/personal/simplecalculator/App.java
+++ src/main/java/personal/simplecalculator/App.java
@@ -1,12 +1,17 @@
 package personal.simplecalculator;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import personal.simplecalculator.graphics.Graphics;
 
 public class App extends Application {
+    Graphics graphics = new Graphics();
+
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setScene(new Scene(graphics.getBoard(), 800, 800));
+        stage.show();
     }
 
     public static void main(String[] args) {
