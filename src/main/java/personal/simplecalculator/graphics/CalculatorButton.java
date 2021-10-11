717ab8a627f0f710507a902a3a50da44773d2115
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-11 13:41:29 +0200

    Added Action class to abstract class to work with functions

--- src/main/java/personal/simplecalculator/graphics/CalculatorButton.java
+++ src/main/java/personal/simplecalculator/graphics/CalculatorButton.java
@@ -2,19 +2,23 @@ package personal.simplecalculator.graphics;
 
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import personal.simplecalculator.functions.Action;
 
 public abstract class CalculatorButton {
     protected final Button button;
+    protected final Action action;
     protected final Text text;
 
     protected CalculatorButton(int number, Text text){
         button = new Button(String.valueOf(number));
+        action = Action.getInstance();
         this.text = text;
         setupButton();
     }
 
     protected CalculatorButton(String value, Text text){
         button = new Button(value);
+        action = Action.getInstance();
         this.text = text;
         setupButton();
     }
