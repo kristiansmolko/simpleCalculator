dce3fae2144f7ac6e3e81391a751a49d1747e712
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-11 13:18:22 +0200

    Added FunctionalCalculatorButton for functions

---
+++ src/main/java/personal/simplecalculator/graphics/FunctionalCalculatorButton.java
@@ -0,0 +1,40 @@
+package personal.simplecalculator.graphics;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class FunctionalCalculatorButton extends CalculatorButton{
+    protected FunctionalCalculatorButton(String value, Text text) {
+        super(value, text);
+        setStyle();
+        setActionEvent();
+    }
+
+    private void setStyle(){
+        button.setBackground(new Background(new BackgroundFill(Color.GRAY, new CornerRadii(5.0), new Insets(0))));
+    }
+
+    private void setActionEvent(){
+        if (!button.getText().equals("C") && !button.getText().equals("="))
+            button.setOnAction(functionalActionEvent());
+        else if (button.getText().equals("C"))
+            button.setOnAction(clearDisplay());
+        else {
+            //TODO add method to calculate result
+        }
+    }
+
+    private EventHandler<ActionEvent> functionalActionEvent(){
+        return actionEvent -> text.setText(text.getText() + button.getText());
+    }
+
+    private EventHandler<ActionEvent> clearDisplay(){
+        return actionEvent -> text.setText("");
+    }
+}
