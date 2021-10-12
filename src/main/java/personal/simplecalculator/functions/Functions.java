c5a26245105399d6b22118ec127281ef5091fde2
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-12 09:02:06 +0200

    Added checking for first negative value, refactored addFunctionsFromExercise and getResult method

--- src/main/java/personal/simplecalculator/functions/Functions.java
+++ src/main/java/personal/simplecalculator/functions/Functions.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Functions {
     private final List<Function> functionList;
     private String exercise;
+    private int[] numbers = new int[2];
 
     public Functions(){
         functionList = new ArrayList<>();
@@ -16,19 +17,27 @@ public class Functions {
     }
 
     private void addFunctionsFromExercise(){
-        if (exercise.contains("+")) functionList.add(Function.ADDING);
-        if (exercise.contains("-")) functionList.add(Function.SUBSTRACT);
-        if (exercise.contains("*")) functionList.add(Function.MULTIPLY);
-        if (exercise.contains("/")) functionList.add(Function.DIVIDE);
+        for (var characterNumber = 0; characterNumber < exercise.length(); ++characterNumber){
+            var character = exercise.charAt(characterNumber);
+            switch(character){
+                case '+' -> functionList.add(Function.ADDING);
+                case '-' -> functionList.add(Function.SUBTRACT);
+                case '*' -> functionList.add(Function.MULTIPLY);
+                case '/' -> functionList.add(Function.DIVIDE);
+            }
+        }
     }
 
     private int[] processExercise(){
         addFunctionsFromExercise();
+        checkForFirstNegativeNumberAndAdd();
         if (isSingleFunction()) {
             var operand = functionList.get(0).getOperand();
             var temp =exercise.split(operand);
-            var numbers = new int[2];
-            numbers[0] = Integer.parseInt(temp[0]);
+            if (numbers[0] == 0) {
+                numbers = new int[2];
+                numbers[0] = Integer.parseInt(temp[0]);
+            }
             numbers[1] = Integer.parseInt(temp[1]);
             return numbers;
         }
@@ -36,16 +45,38 @@ public class Functions {
     }
 
     public int getResult(){
-        var numbers = processExercise();
+        var exerciseNumbers = processExercise();
         if (isSingleFunction()){
             switch(functionList.get(0).getValue()){
-                case 0: return numbers[0] + numbers[1];
-                case 1: return numbers[0] - numbers[1];
-                case 2: return numbers[0] * numbers[1];
-                case 3: return numbers[0] / numbers[1];
+                case 0: return exerciseNumbers[0] + exerciseNumbers[1];
+                case 1: return exerciseNumbers[0] - exerciseNumbers[1];
+                case 2: return exerciseNumbers[0] * exerciseNumbers[1];
+                case 3: return exerciseNumbers[0] / exerciseNumbers[1];
             }
         }
         return 0;
+    }
+
+    private boolean checkForNegativeFirstNumber(){
+        return exercise.charAt(0) == '-';
+    }
+
+    private void addFirstNegativeNumber(){
+        var operand = functionList.get(0).getOperand();
+        var temp = exercise.split(operand);
+        numbers[0] = -(Integer.parseInt(temp[0]));
+    }
+
+    private void removeSubtraction() {
+        functionList.remove(Function.SUBTRACT);
+        exercise = exercise.replaceFirst("-", "");
+    }
+
+    private void checkForFirstNegativeNumberAndAdd(){
+        if (checkForNegativeFirstNumber()) {
+            removeSubtraction();
+            addFirstNegativeNumber();
+        }
     }
 
     private boolean isSingleFunction(){
